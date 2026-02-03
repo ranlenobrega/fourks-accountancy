3048b640a0353bcb3c8dde9fd2dee22ca15f8448
ranlenobrega <82420685+ranlenobrega@users.noreply.github.com>
2026-02-03 15:30:55 +0000

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Hero from '@/components/hero'
+import ServicesSection from '@/components/services-section'
+import WhoWeHelp from '@/components/who-we-help'
+import ProcessSection from '@/components/process-section'
+import FaqSection from '@/components/faq-section'
+import CtaSection from '@/components/cta-section'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <ServicesSection />
+      <WhoWeHelp />
+      <ProcessSection />
+      <FaqSection />
+      <CtaSection />
+    </>
+  )
+}
